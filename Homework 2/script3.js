b2e5869e585ba28123c1ce12b69978fc188f3504
garrhetsampson <70622856+garrhetsampson@users.noreply.github.com>
2022-02-15 00:32:31 -0700

    Add files via upload

---
+++ Homework 2/script3.js
@@ -0,0 +1,4 @@
+// Example of Using the document.write
+// function to place text in an HTML document
+
+document.write("<h1>3 of your favorite artists and their cultural importance:</h1><p>Banksy: Street Artist. Christian Watson: Vintage inspired hand lettering artist. Mark Kern: Creator of Warcraft and Starcraft. ");
